7e63363aa72a695bb9370aa0c303f698b77b7f3f
Indira velidandla <insravan@microsoft.com>
2023-08-16 18:07:43 -0700

    fixed review comments

--- Project/src/MakeCall/MakeCall.js
+++ Project/src/MakeCall/MakeCall.js
@@ -505,7 +505,7 @@ this.currentCall = this.callAgent.join({groupId: <GUID>}, this.callOptions);
 // open the participants roster, then click on the 'Share Invite' button and then click on 'Copy link meeting' button.
 this.currentCall = this.callAgent.join({meetingLink: <meeting link>}, this.callOptions);
 // Join a Teams meeting using a meeting id.
-this.currentCall = this.callAgent.join({meetingId: <meeting id> , passcode: <passcode>}, this.callOptions);
+this.currentCall = this.callAgent.join({meetingId: <meeting id> , passcode (optional): <passcode>}, this.callOptions);
 // Join a Teams meeting using meeting coordinates. Coordinates can be derived from the meeting link
 // Teams meeting link example
 const meetingLink = 'https://teams.microsoft.com/l/meetup-join/19:meeting_NjNiNzE3YzMtYzcxNi00ZGQ3LTk2YmYtMjNmOTE1MTVhM2Jl@thread.v2/0?context=%7B%22Tid%22:%2272f988bf-86f1-41af-91ab-2d7cd011db47%22,%22Oid%22:%227e353a91-0f71-4724-853b-b30ee4ca6a42%22%7D'
@@ -955,7 +955,7 @@ this.deviceManager.on('selectedSpeakerChanged', () => { console.log(this.deviceM
                                             componentRef={(val) => this.meetingId = val} />
                                         <TextField className="mb-3"
                                             disabled={this.state.call || !this.state.loggedIn}
-                                            label="Meeting passcode"
+                                            label="Meeting passcode (optional)"
                                             componentRef={(val) => this.passcode = val} />
                                         <div> Or enter meeting coordinates (Thread Id, Message Id, Organizer Id, and Tenant Id)</div>
                                         <TextField disabled={this.state.call || !this.state.loggedIn}
