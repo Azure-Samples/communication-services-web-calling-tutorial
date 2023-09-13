04389e86e885599f00ddd04f4674af5b3fd2dd0d
Elavarasi Duraipandian Chenthoorpan <elavarasid@microsoft.com>
2023-09-13 10:49:39 -0700

    commit config.

--- Project/oAuthConfig.js
+++ Project/oAuthConfig.js
@@ -1,13 +1,13 @@
 const authConfig = {
     auth: {
-        clientId: 'ENTER_CLIENT_ID',
-        authority: 'https://login.microsoftonline.com/ENTER_TENANT_ID'
-    }
+            clientId: '5ea2529c-3327-47e0-a362-c75a829fb6f2',
+            authority: 'https://login.microsoftonline.com/bc61f4fc-26d7-411e-91a9-4c14691dabdf'
+        }
 };
  // Add here scopes for id token to be used at MS Identity Platform endpoints.
-const authScopes = {
-    popUpLogin: [],
-    m365Login: []
+ const authScopes = {
+    popUpLogin: ["https://auth.msft.communication.azure.com/Teams.ManageCalls", "https://auth.msft.communication.azure.com/Teams.ManageChats"],
+    m365Login: ['https://auth.msft.communication.azure.com/.default']
 };
 
 module.exports = {authConfig, authScopes }
