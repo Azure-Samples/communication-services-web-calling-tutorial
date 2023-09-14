5f55cce7ca496b50b518bc92344fe28d706148b9
Christopher Whilar <chwhilar@microsoft.com>
2023-09-14 17:38:49 -0400

    Fix

--- cypress/e2e/1-getting-started/todo.cy.js
+++ cypress/e2e/1-getting-started/todo.cy.js
@@ -21,11 +21,13 @@ describe('example to-do app', () => {
   })
 
   it('1:1 audio call', () => {
-    const user0Div = cy.get('[id=user-0]');
-    user0Div.find('[id=login-button]').click();
-    cy.wait(15000);
-    user0Div.find('[id=acs-login-success-message]').should('contain', 'Congrats! You\'ve provisioned an ACS user identity');
-    cy.wait(5000);
+    cy.get('[id=user-0]')
+      .find('[id=login-button]')
+      .click()
+      .wait(15000)
+      .find('[id=acs-login-success-message]')
+      .should('contain', 'Congrats! You\'ve provisioned an ACS user identity')
+      .wait(5000);
   });
 
   // it('displays two todo items by default', () => {
