89d10a635f6e5a6860c25637ddbeafc73583f3b9
Christopher Whilar <chwhilar@microsoft.com>
2023-09-15 14:23:38 -0400

    res

--- cypress/e2e/utils/utils.js
+++ cypress/e2e/utils/utils.js
@@ -5,25 +5,33 @@ export async function loginUsers(numberOfUsers) {
         .click();
     }
 
-    for (let i = 0; i < numberOfUsers; i++) {
-        cy.get(`[id=user-${i}]`)
-            .find('[id=login-button]')
-            .click();
-        cy.wait(15000);
-
-        cy.get(`[id=user-${i}]`)
-            .find('[id=acs-login-success-message]')
-            .should('contain', 'Congrats! You\'ve provisioned an ACS user identity');
-
-        cy.get(`[id=user-${i}]`)
-            .find('[id=acs-login-success-message]')
-            .should('contain', 'Congrats! You\'ve provisioned an ACS user identity');
-
-        const text = await cy.get(`[id=user-${i}]`)
-            .find('[id=acs-identity]')
-            .invoke('text');
-        ids.push(text)
-    }
+    await new Promise((res, rej) => {
+        for (let i = 0; i < numberOfUsers; i++) {
+            cy.get(`[id=user-${i}]`)
+                .find('[id=login-button]')
+                .click();
+            cy.wait(15000);
+    
+            cy.get(`[id=user-${i}]`)
+                .find('[id=acs-login-success-message]')
+                .should('contain', 'Congrats! You\'ve provisioned an ACS user identity');
+    
+            cy.get(`[id=user-${i}]`)
+                .find('[id=acs-login-success-message]')
+                .should('contain', 'Congrats! You\'ve provisioned an ACS user identity');
+    
+            cy.get(`[id=user-${i}]`)
+                .find('[id=acs-identity]')
+                .invoke('text')
+                .then(text => {
+                    cy.log('The val text', text);
+                    ids.push(text)
+                    if (i == numberOfUsers - 1) {
+                        res();
+                    }
+                });
+        }
+    });
 
     cy.log('The ids:', ids[0], ids[1]);
     return ids;
