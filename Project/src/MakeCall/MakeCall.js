9a2960722bfef3d0225dac1fa9873e7b3531c335
Christopher Whilar <chwhilar@microsoft.com>
2025-02-26 13:48:28 -0500

    remote ufds start sending

--- Project/src/MakeCall/MakeCall.js
+++ Project/src/MakeCall/MakeCall.js
@@ -198,8 +198,8 @@ export default class MakeCall extends React.Component {
 
                         call.feature(Features.UserFacingDiagnostics).media.on('diagnosticChanged', diagnosticChangedListener);
                         call.feature(Features.UserFacingDiagnostics).network.on('diagnosticChanged', diagnosticChangedListener);
-                        call.feature(Features.UserFacingDiagnostics).remote?.on('diagnosticChanged', remoteDiagnosticChangedListener);
-                        window.ufds = call.feature(Features.UserFacingDiagnostics);
+                        call.feature(Features.UserFacingDiagnostics).remote.startSendingDiagnostics?.();
+                        call.feature(Features.UserFacingDiagnostics).remote.on('diagnosticChanged', remoteDiagnosticChangedListener);
                     });
 
                     e.removed.forEach(call => {
