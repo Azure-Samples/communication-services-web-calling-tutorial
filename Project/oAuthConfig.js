6ec8ebb55fef8c2e1062d9added1cfc6a0a25cf7
Sharif Rahaman <srahaman@microsoft.com>
2025-09-24 10:49:45 -0700

    Move auth config to server side

--- Project/oAuthConfig.js
+++ Project/oAuthConfig.js
@@ -1,13 +1,19 @@
 const authConfig = {
-    auth: {
-        clientId: 'ENTER_CLIENT_ID',
-        authority: 'https://login.microsoftonline.com/ENTER_TENANT_ID'
+    configuration: {
+        auth: {
+            clientId: 'ENTER_CLIENT_ID',
+            authority: 'https://login.microsoftonline.com/common'
+        }
+    },
+    scopes: {
+        m365Login: [
+            "https://auth.msft.communication.azure.com/.default"
+        ],
+        popUpLogin: [
+            "https://auth.msft.communication.azure.com/Teams.ManageCalls",
+            "https://auth.msft.communication.azure.com/Teams.ManageChats"
+        ]
     }
-};
- // Add here scopes for id token to be used at MS Identity Platform endpoints.
-const authScopes = {
-    popUpLogin: [],
-    m365Login: []
 };
 
 const entraCredentialConfig = {
@@ -16,4 +22,4 @@ const entraCredentialConfig = {
     resourceEndpoint: 'ACS_RESOURCE_ENDPOINT' // e.g., 'https://contoso.unitedstates.communication.azure.com/'
 };
 
-module.exports = {authConfig, authScopes, entraCredentialConfig }
+module.exports = { authConfig, entraCredentialConfig }
