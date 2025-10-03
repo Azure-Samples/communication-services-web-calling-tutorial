4dc17799e149286d197b667d9612bdfcf87c3674
Yassir Bisteni <bisteni112@gmail.com>
2025-10-03 08:31:15 -0600

    Added Voice Isolation support

--- Project/src/MakeCall/MakeCall.js
+++ Project/src/MakeCall/MakeCall.js
@@ -83,6 +83,7 @@ export default class MakeCall extends React.Component {
             },
             preCallDiagnosticsResults: {},
             isTeamsUser: false,
+            headsetEnhancement: false,
             identityMri: undefined
         };
 
@@ -129,6 +130,10 @@ export default class MakeCall extends React.Component {
                 this.tokenCredential = tokenCredential;
                 setLogLevel('verbose');
 
+                this.setState({ headsetEnhancement: userDetails.headsetEnhancement });
+
+                console.log("MakeCall::handleLogIn, headsetEnhancement: ", userDetails.headsetEnhancement);
+
                 const proxyConfiguration = userDetails.proxy.useProxy ? { url: userDetails.proxy.url } : undefined;
                 const turnConfiguration = userDetails.customTurn.useCustomTurn ? userDetails.customTurn.turn : undefined;
                 this.callClient = new CallClient({
@@ -141,6 +146,9 @@ export default class MakeCall extends React.Component {
                     networkConfiguration: {
                         proxy: proxyConfiguration,
                         turn: turnConfiguration
+                    },
+                    audioOptions: {
+                        headsetEnhancement: userDetails.headsetEnhancement
                     }
                 });
 
